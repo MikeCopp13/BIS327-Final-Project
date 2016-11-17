d31454eef0dc2b228eacc5d77dd6b23476038b15
rochford77 <ryanrochford7@gmail.com>
2016-11-16 22:46:35 -0500

    scrapped bootstrap. got images to work

---
+++ js/base.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+    //$(".container").css("height", $( window ).height());
+
+
+});
+
+
+//syntax is $("selector").call();
+//http://api.jquery.com/height/
+//http://stackoverflow.com/questions/6718484/how-to-add-property-to-existing-class
